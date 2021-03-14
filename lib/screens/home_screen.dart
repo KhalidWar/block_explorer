530d47994cea9b94d97530e87327c678d72064ad
Khalid War <git@khalidwar.com>
2021-03-14 09:11:44 -0500

    Added header and footer to result screen Improved search functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:block_explorer/screens/error_screen.dart';
 import 'package:block_explorer/screens/result_screen.dart';
 import 'package:block_explorer/services/form_validation.dart';
+import 'package:block_explorer/state_management/providers.dart';
 import 'package:block_explorer/state_management/search_state_management.dart';
 import 'package:block_explorer/widgets/header.dart';
 import 'package:flutter/cupertino.dart';
@@ -22,6 +24,35 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
+    void search() async {
+      if (formKey.currentState.validate()) {
+        await context
+            .read(apiServiceProvider)
+            .getAccountBalance(textEditingController.text.trim())
+            .then((value) {
+          if (value == null) {
+            Navigator.of(context)
+                .push(
+                  MaterialPageRoute(
+                    builder: (context) => ErrorScreen(),
+                  ),
+                )
+                .whenComplete(() => textEditingController.clear());
+          } else {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  context.read(searchStateManagementProvider).walletAddress =
+                      textEditingController.text.trim();
+                  return ResultScreen();
+                },
+              ),
+            ).whenComplete(() => textEditingController.clear());
+          }
+        });
+      }
+    }
+
     return GestureDetector(
       onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
       child: Scaffold(
@@ -43,6 +74,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       controller: textEditingController,
                       validator: (input) =>
                           FormValidation().validateSearchInput(input),
+                      onFieldSubmitted: (input) => search(),
                       decoration: InputDecoration(
                         contentPadding: EdgeInsets.all(10),
                         hintText: 'Search for anything',
@@ -57,27 +89,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(height: size.height * 0.03),
                 ElevatedButton(
                   child: Text('Search'),
-                  onPressed: () async {
-                    if (formKey.currentState.validate()) {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) {
-                            context
-                                    .read(searchStateManagementProvider)
-                                    .walletAddress =
-                                textEditingController.text.trim();
-
-                            return ResultScreen();
-                          },
-                        ),
-                      ).whenComplete(() => textEditingController.clear());
-                    }
-                  },
+                  onPressed: () => search(),
                 ),
               ],
             ),
-            // Text(test),
-            Container(),
             Footer(),
           ],
         ),
--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,6 +1,8 @@
 import 'package:block_explorer/models/account_balance.dart';
 import 'package:block_explorer/models/tx_model.dart';
 import 'package:block_explorer/state_management/search_state_management.dart';
+import 'package:block_explorer/widgets/footer.dart';
+import 'package:block_explorer/widgets/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -26,47 +28,52 @@ class ResultScreen extends ConsumerWidget {
 
     return Scaffold(
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            walletBalanceAsyncValue.when(
-              loading: () => CircularProgressIndicator(),
-              data: (data) => Text(data.result),
-              error: (error, stackTrace) => Container(),
-            ),
-            Container(
-              padding: EdgeInsets.only(right: size.width * 0.1),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Transactions',
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                  txListAsyncValue.when(
-                    loading: () => CircularProgressIndicator(),
-                    data: (data) {
-                      return ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: data.result.length,
-                        itemBuilder: (_, index) {
-                          final transaction = data.result[index];
+            Header(),
+            Column(
+              children: [
+                walletBalanceAsyncValue.when(
+                  loading: () => CircularProgressIndicator(),
+                  data: (data) => Text(data.result),
+                  error: (error, stackTrace) => Container(),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: size.width * 0.1),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Transactions',
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                      txListAsyncValue.when(
+                        loading: () => CircularProgressIndicator(),
+                        data: (data) {
+                          return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: 50,
+                            itemBuilder: (_, index) {
+                              final transaction = data.result[index];
 
-                          return ListTile(
-                            dense: true,
-                            leading: Icon(Icons.done),
-                            title: Text(transaction.confirmations),
-                            subtitle: Text(transaction.from),
+                              return ListTile(
+                                dense: true,
+                                leading: Icon(Icons.done),
+                                title: Text(transaction.confirmations),
+                                subtitle: Text(transaction.from),
+                              );
+                            },
                           );
                         },
-                      );
-                    },
-                    error: (error, stackTrace) => Container(),
+                        error: (error, stackTrace) => Container(),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
+            Footer(),
           ],
         ),
       ),
